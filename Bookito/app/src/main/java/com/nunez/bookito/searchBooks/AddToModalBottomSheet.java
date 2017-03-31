f164a3b9a8f6b31217818f6f1398a621ba918f74
Paul Nunez <paulnunezm@gmail.com>
2017-03-31 00:31:19 -0400

    added: content descriptions for the 'add to' modal bottom sheet.
    And refractored the class

--- Bookito/app/src/main/java/com/nunez/bookito/searchBooks/AddToModalBottomSheet.java
+++ Bookito/app/src/main/java/com/nunez/bookito/searchBooks/AddToModalBottomSheet.java
@@ -1,5 +1,6 @@
 package com.nunez.bookito.searchBooks;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
@@ -14,43 +15,66 @@ import com.nunez.bookito.repositories.FirebaseNodes;
  * Created by paulnunez on 3/24/17.
  */
 
-public class AddToModalBottomSheet extends BottomSheetDialogFragment {
+public class AddToModalBottomSheet extends BottomSheetDialogFragment implements View.OnClickListener {
   OnModalOptionSelected listener;
 
-  public interface OnModalOptionSelected {
+
+  interface OnModalOptionSelected {
     void onModalOptionSelected(String selectedItem);
   }
 
-  static AddToModalBottomSheet newInstace(){
+  static AddToModalBottomSheet newInstace() {
     return new AddToModalBottomSheet();
   }
 
-  public AddToModalBottomSheet(){}
+  public AddToModalBottomSheet() {
+  }
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//    return super.onCreateView(inflater, container, savedInstanceState);
     View v = inflater.inflate(R.layout.search_activity_add_to_modal, container, false);
 
-    v.findViewById(R.id.btn_wishlist).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        listener.onModalOptionSelected(FirebaseNodes.WISHLIST);
-      }
-    });
+    Resources res = getContext().getResources();
 
-    v.findViewById(R.id.btn_my_books).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        listener.onModalOptionSelected(FirebaseNodes.MY_BOOKS);
-      }
-    });
+    container.setContentDescription(res.getString(R.string.search_activity_add_to));
+
+    View btnWishList = v.findViewById(R.id.btn_wishlist);
+    View btnMyBook   = v.findViewById(R.id.btn_my_books);
+
+    // Set content descriptions
+    setButtonContentDescription(btnWishList, R.string.search_activity_add_to);
+    setButtonContentDescription(btnMyBook, R.string.search_activity_my_books);
+
+    // Set listeners
+    btnWishList.setOnClickListener(this);
+    btnMyBook.setOnClickListener(this);
 
     return v;
   }
 
-  public void setItemSelectedListener(OnModalOptionSelected listener){
+  private void setButtonContentDescription(View v, int stringResouce) {
+    v.setContentDescription(
+        getResources().getString(R.string.accessibility_search_activity_add_to, // Format string
+            getResources().getString(stringResouce))
+    );
+  }
+
+  public void setItemSelectedListener(OnModalOptionSelected listener) {
     this.listener = listener;
   }
+
+  // Implementations
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.btn_wishlist:
+        listener.onModalOptionSelected(FirebaseNodes.WISHLIST);
+        break;
+      case R.id.btn_my_books:
+        listener.onModalOptionSelected(FirebaseNodes.MY_BOOKS);
+        break;
+    }
+  }
+
 }
