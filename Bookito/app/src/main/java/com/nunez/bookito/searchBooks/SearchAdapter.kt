f8d006aa513eca7b8eddabaa891be209b15b7fcc
Paul Nunez <paulnunezm@gmail.com>
2017-04-09 12:22:17 -0400

    edited: SearchAdapter to Kotlin class.
    Edited the search adapter to kotlin to be ablet to use kotlin languages features,
    in this class we saved up to 60 lines of code implementing this language.

---
+++ Bookito/app/src/main/java/com/nunez/bookito/searchBooks/SearchAdapter.kt
@@ -0,0 +1,58 @@
+package com.nunez.bookito.searchBooks
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nunez.bookito.R
+import com.nunez.bookito.entities.BookWrapper
+import com.nunez.bookito.loadImage
+import com.nunez.bookito.setTextAndContentDescription
+import kotlinx.android.synthetic.main.item_search.view.*
+
+/**
+ * Created by paulnunez on 3/23/17.
+ */
+
+class SearchAdapter(private var bookWrappers: ArrayList<BookWrapper>?, private val listener: SearchViewHolder.SearchBookListener) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        context = parent.context
+        val searchViewItem = LayoutInflater.from(context).inflate(R.layout.item_search, parent, false)
+
+        return SearchViewHolder(searchViewItem, listener)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) = with(holder) {
+        setBookWrapper(bookWrappers!![position])
+    }
+
+    override fun getItemCount(): Int = bookWrappers!!.size
+
+    fun clear() {
+        bookWrappers = ArrayList<BookWrapper>()
+        notifyDataSetChanged()
+    }
+
+    class SearchViewHolder(itemView: View, private val listener: SearchViewHolder.SearchBookListener) : RecyclerView.ViewHolder(itemView) {
+
+        interface SearchBookListener {
+            fun onAddToClickListener(bookWrapper: BookWrapper)
+        }
+
+        fun setBookWrapper(item: BookWrapper) {
+            val book = item.getBook()
+
+            with(book) {
+                itemView.txt_title.setTextAndContentDescription(title)
+                itemView.txt_author.setTextAndContentDescription(author.name)
+                itemView.img_cover.loadImage(imageUrl)
+                itemView.rating_stars.contentDescription = item.averageRating
+                itemView.rating_stars.rating = item.averageRating.toFloat()
+                itemView.btn_add_to.setOnClickListener { listener.onAddToClickListener(item) }
+            }
+        }
+    }
+}
